0f702e8e403cd0e1fc92d06a9c74f95c1b0af589
Immodicus <53706492+Immodicus@users.noreply.github.com>
2022-12-06 13:58:42 +0000

    minor fix

--- video_cmp.cpp
+++ video_cmp.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 {
     if(argc < 3)
     {
-        std::cerr << "Usage: video_cmp originalVideo compressedVideo";
+        std::cerr << "Usage: video_cmp originalVideo compressedVideo\n";
         return EXIT_FAILURE;
     }
 
